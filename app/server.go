07fdd57efc1a3c985259154b7a6d47c468872598
wangshu24 <trandinhphuc1997@gmail.com>
2024-12-04 23:55:07 +1300

    add connection write

--- app/server.go
+++ app/server.go
@@ -21,9 +21,12 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	// buff := []byte{}
+	conn.Write([]byte("000007"))
 }
